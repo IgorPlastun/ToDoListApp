157037700bd0a328272fcde7599de3451a3562e3
IgorPlastun <igorplastun@rambler.ru>
2020-11-23 19:59:43 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ToDoListItem} from "../app/to-do-list-item"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lesson1';
+  title = 'lesson1';  
+  getTitle = function():string{
+    return this.title;
+  }
 }
